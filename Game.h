af6a11b195ca4c13ea58043a07e348aa7c571707
YdeaGames <ydeagames@gmail.com>
2019-11-06 09:53:24 +0900

    First Commit

---
+++ Game.h
@@ -0,0 +1,61 @@
+//
+// Game.h
+//
+
+#pragma once
+
+#include "DeviceResources.h"
+#include "StepTimer.h"
+
+
+// A basic game implementation that creates a D3D11 device and
+// provides a game loop.
+class Game final : public DX::IDeviceNotify
+{
+public:
+	static const int SCREEN_W = 1280;
+	static const int SCREEN_H = 720;
+
+public:
+
+    Game() noexcept(false);
+
+    // Initialization and management
+    void Initialize(HWND window, int width, int height);
+
+    // Basic game loop
+    void Tick();
+
+    // IDeviceNotify
+    virtual void OnDeviceLost() override;
+    virtual void OnDeviceRestored() override;
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowMoved();
+    void OnWindowSizeChanged(int width, int height);
+
+    // Properties
+    void GetDefaultSize( int& width, int& height ) const;
+
+	void ChangeFullscreen(BOOL flag);
+
+private:
+
+    void Update(DX::StepTimer const& timer);
+    void Render();
+
+    void Clear();
+
+    void CreateDeviceDependentResources();
+    void CreateWindowSizeDependentResources();
+
+    // Device resources.
+    std::unique_ptr<DX::DeviceResources>    m_deviceResources;
+
+    // Rendering loop timer.
+    DX::StepTimer                           m_timer;
+};
