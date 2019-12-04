cc03f0f9cfcf455b859c2ed34a960c2814e2b70c
YdeaGames <ydeagames@gmail.com>
2019-12-04 14:20:07 +0900

    Object.cpp

---
+++ Object.cpp
@@ -0,0 +1,153 @@
+//======================================================
+// File Name	: Object.cpp
+// Summary		: オブジェクトクラス
+// Date			: 2019/12/02
+// Author		: Takafumi Ban
+//======================================================
+#include "pch.h"
+#include "Object.h"
+#include "DeviceResources.h"
+#include <CommonStates.h>
+#include "GameWindow.h"
+#include "GameContext.h"
+#include "Camera.h"
+
+Object::Object()
+	: m_model(nullptr)
+	, m_dir(0)
+	, m_weight(0.0f)
+	, m_coefficientOfFriction(0.0f)
+	, m_radius(0.0f)
+	, m_width(0.0f)
+	, m_height(0.0f)
+	, m_hitFlag(false)
+	, m_displayFlag(true)
+	, m_activeFlag(true)
+	, m_x(0)
+	, m_y(0)
+{
+}
+
+void Object::Update(float elapsedTime)
+{
+	if (!m_activeFlag) return;
+
+	// 位置に速度を足す
+	m_pos += m_vel;
+}
+
+void Object::Render()
+{
+	GameWindow* gameWindow = GameContext::Get<GameWindow>();
+	DX::DeviceResources* deviceResources = GameContext::Get<DX::DeviceResources>();
+	Camera* camera = gameWindow->GetCamera();
+
+	if (!gameWindow || !m_model || !m_displayFlag) return;
+	
+	// ワールド行列を作成
+	DirectX::SimpleMath::Matrix world = DirectX::SimpleMath::Matrix::CreateTranslation(m_pos);
+
+	// モデルの描画
+	m_model->Draw(deviceResources->GetD3DDeviceContext(), *GameContext::Get<DirectX::CommonStates>()
+		, world, camera->getViewMatrix(), camera->getProjectionMatrix());
+}
+
+float Object::GetHitForce()
+{
+	float force = 0.1f;
+
+	// 連続ヒット時の速度の影響が大きすぎたので速度を抑える
+	float v = m_vel.Length();
+	if (v >= 0.1f) v = 0.1f;
+	force += m_weight * v;
+
+	return force;
+}
+
+void Object::AddForce(float angle, float force)
+{
+	// 進行方向ベクトル
+	DirectX::SimpleMath::Vector3 dir(0.0f, 0.0f, -1.0f);
+
+	// 自機の向きベクトルを求める
+	DirectX::SimpleMath::Matrix rotY = DirectX::SimpleMath::Matrix::CreateRotationY(angle);
+	dir = DirectX::SimpleMath::Vector3::Transform(dir, rotY);
+
+	// 加速度
+	m_acc = dir * (force / m_weight);
+
+	// 速度に加速度を足す
+	m_vel += m_acc;
+}
+
+void Object::Friction(float elapsedTime)
+{
+	// 摩擦力 = 摩擦係数 × 重力加速度 × 質量
+	float frictionForce = m_coefficientOfFriction * m_weight * GameWindow::GRAVITY;
+
+	// 摩擦力によって速度を落とす
+	float v = (frictionForce / m_weight) * elapsedTime;
+
+	if (m_vel.LengthSquared() > v * v)
+	{
+		v = m_vel.Length() - v;
+		m_vel.Normalize();
+		m_vel *= v;
+	}
+	else
+	{
+		m_vel = DirectX::SimpleMath::Vector3::Zero;
+	}
+}
+
+int Object::GetDir(Object * object)
+{
+	// ターゲットへのベクトルを求める
+	DirectX::SimpleMath::Vector3 v = object->GetPosition() - m_pos;
+
+	// 内積を使い一番近い角度を求める
+	DirectX::SimpleMath::Matrix rotY = DirectX::SimpleMath::Matrix::CreateRotationY(DirectX::XMConvertToRadians(45.0f));
+	DirectX::SimpleMath::Vector3 dirVec(0.0f, 0.0f, -1.0f);
+	int dir = 0;
+	float maxVal = dirVec.Dot(v);
+	// 内積が一番大きい方向が近い
+	for (int i = 0; i < 7; i++)
+	{
+		dirVec = DirectX::SimpleMath::Vector3::Transform(dirVec, rotY);
+		float val = dirVec.Dot(v);
+		if (val > maxVal)
+		{
+			dir = i + 1;
+			maxVal = val;
+		}
+	}
+	return dir;
+}
+
+bool Object::CheckFloor()
+{
+	if (m_floorCheckFunction)
+	{
+		return m_floorCheckFunction(this);
+	}
+	return true;
+}
+
+void Object::AddScore(int score)
+{
+	if (m_addScoreFunction)
+	{
+		m_addScoreFunction(score);
+	}
+}
+
+void Object::SetCheckFloorFunction(std::function<bool(Object*)> func)
+{
+	m_floorCheckFunction = func;
+}
+
+void Object::SetAddScoreFunction(std::function<void(int score)> func)
+{
+	m_addScoreFunction = func;
+}
+
---
+++ Object.h
@@ -0,0 +1,147 @@
+//======================================================
+// File Name	: Object.h
+// Summary		: オブジェクトクラス
+// Date			: 2019/12/02
+// Author		: Takafumi Ban
+//======================================================
+#pragma once
+
+#include "GameObject.h"
+#include <Model.h>
+
+class GameWindow;
+
+// オブジェクトクラス
+class Object : public GameObject
+{
+public:
+
+	// オブジェクトＩＤ
+	enum OBJECT_ID
+	{
+		NONE,			// なし
+		PLAYER,			// プレイヤー
+		POWERUP_PARTS,	// パワーアップパーツ
+		JUMP_PARTS,		// ジャンプパーツ
+		ENEMY_1,		// 敵１
+		ENEMY_2,		// 敵２
+	};
+
+public:
+	// コンストラクタ
+	Object();
+
+	// 更新
+	void Update(float elapsedTime) override;
+
+	// 描画
+	void Render() override;
+
+	// オブジェクトＩＤを取得する関数
+	virtual Object::OBJECT_ID GetID() = 0;
+
+	// 位置を取得する関数
+	const DirectX::SimpleMath::Vector3& GetPosition() { return m_pos; }
+
+	// 半径を取得する関数
+	float GetRadius() { return m_radius; }
+
+	// 衝突フラグの設定関数
+	void SetHitFlag(bool flag) { m_hitFlag = flag; }
+
+	// 衝突フラグの取得関数
+	bool GetHitFlag() { return m_hitFlag; }
+
+	// 幅を取得する関数
+	float GetWidth() { return m_width; }
+
+	// 高さを取得する関数
+	float GetHeight() { return m_height; }
+
+	// 質量を取得する関数
+	float GetWeight() { return m_weight; }
+
+	// 床のチェック関数の登録関数
+	void SetCheckFloorFunction(std::function<bool(Object* object)> func);
+
+	// 得点を加算する関数の登録関数
+	void SetAddScoreFunction(std::function<void(int score)> func);
+
+	// 表示のON/OFFする関数
+	void SetDisplayFlag(bool flag) { m_displayFlag = flag; }
+
+	// 表示のON/OFFを取得する関数
+	bool GetDisplayFlag() { return m_displayFlag; }
+
+	// 更新関数を実行するかどうか設定する関数
+	void SetActive(bool flag) { m_activeFlag = flag; }
+
+	// 衝突した時に相手に与える力を取得する関数
+	virtual float GetHitForce();
+
+	// リセット関数
+	virtual void Reset() {}
+
+protected:
+	// 力を加える関数
+	void AddForce(float angle, float force);
+
+	// 摩擦により速度を減速する関数
+	void Friction(float elapsedTime);
+
+	// ターゲットの方向を返す関数（真上を０として、逆時計回転で８方向）
+	int GetDir(Object* object);
+
+	// 床のチェック関数（false：床なし）
+	bool CheckFloor();
+
+	// 得点を加算する関数
+	void AddScore(int score);
+
+protected:
+
+	// モデルデータへのポインタ
+	DirectX::Model* m_model;
+
+	// 向いている方向（８方向で表す。各ビットで押された方向を表す。）
+	int m_dir;
+
+	// 質量
+	float m_weight;
+
+	// 床との摩擦係数（μ）
+	float m_coefficientOfFriction;
+
+	// 加速度
+	DirectX::SimpleMath::Vector3 m_acc;
+
+	// 速度
+	DirectX::SimpleMath::Vector3 m_vel;
+
+	// 位置
+	DirectX::SimpleMath::Vector3 m_pos;
+
+	// 半径
+	float m_radius;
+
+	// 幅と高さ
+	float m_width, m_height;
+
+	// 衝突フラグ
+	bool m_hitFlag;
+
+	// 床のチェック関数
+	std::function<bool(Object*)> m_floorCheckFunction;
+
+	// 得点を加算する関数
+	std::function<void(int score)> m_addScoreFunction;
+
+	// 表示フラグ
+	bool m_displayFlag;
+
+	// 更新関数を実行するかどうか判断するフラグ
+	bool m_activeFlag;
+
+	// ステージ上の位置
+	int m_x, m_y;
+};
