e13cc85665b7903b9a83e19e88fbfc354dd25368
YdeaGames <ydeagames@gmail.com>
2020-01-23 11:34:54 +0900

    ADX

---
+++ Data/MotosSound/Public/MotosSound_acf.h
@@ -0,0 +1,68 @@
+﻿/*===========================================================================*
+ *  Header file for Atom Config File
+ *  Project            : MotosSound
+ *  Tool Ver.          : Ver.3.40.17
+ *  ACF  Ver.          : Ver.1.20.03
+ *  File Path          : C:/b7n/学校/専門学校2年/workspace/Visual Studio 2015/Projects/3Dプログラム/Motos/Data/MotosSound/Public
+ *  File Name          : MotosSound_acf.h
+ *  File Size          : 4416 bytes
+ *  Date Time          : 2020/01/23 11:33:21
+ *  Target             : Public
+ *  Categories         : 1
+ *  AISAC Controls     : 16
+ *  Global AISACs      : 0
+ *  Voice Limit Groups : 1
+ *  DSP BUS Settings   : 1
+ *  DSP BUS Settings Snapshot  : 0
+ *  DSP BUS Name       : 1
+ *  Game Variables     : 1
+ *  Selectors          : 0
+ *  REACTs             : 0
+ *  Project Comment    : 
+ *===========================================================================*/
+
+#define CRI_MOTOSSOUND_ACF_NUM_CATEGORIES_PER_PLAYBACK (4)
+#define CRI_MOTOSSOUND_ACF_CATEGORYNUM (1)
+#define CRI_MOTOSSOUND_ACF_AISACCONTROLNUM (16)
+#define CRI_MOTOSSOUND_ACF_GLOBALAISACNUM (0)
+#define CRI_MOTOSSOUND_ACF_VOICELIMITGROUPNUM (1)
+#define CRI_MOTOSSOUND_ACF_DSPSETTINGNUM (1)
+#define CRI_MOTOSSOUND_ACF_DSPSETTINGSNAPSHOTNUM (0)
+#define CRI_MOTOSSOUND_ACF_DSPBUSNAMENUM (1)
+#define CRI_MOTOSSOUND_ACF_GAMEVARIABLENUM (1)
+#define CRI_MOTOSSOUND_ACF_SELECTORNUM (0)
+#define CRI_MOTOSSOUND_ACF_REACTNUM (0)
+
+/* Category List (Category ID) */
+#define CRI_MOTOSSOUND_ACF_CATEGORY_CATEGORY_0           ( 0) /*  */
+
+/* AISAC Control List (AISAC Control ID) */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_00      ( 0) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_01      ( 1) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_02      ( 2) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_03      ( 3) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_04      ( 4) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_05      ( 5) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_06      ( 6) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_07      ( 7) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_08      ( 8) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_09      ( 9) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_10      (10) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_11      (11) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_12      (12) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_13      (13) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_14      (14) /*  */
+#define CRI_MOTOSSOUND_ACF_AISACCONTROL_AISACCONTROL_15      (15) /*  */
+
+/* Voice Limit Group (Voice Limit Group Index) */
+#define CRI_MOTOSSOUND_ACF_VOICELIMITGROUP_VOICELIMITGROUP_0    ( 0) /*  */
+
+/* DspSetting List (DspSetting Name) */
+#define CRI_MOTOSSOUND_ACF_DSPSETTING_DSPBUSSETTING_0      "DspBusSetting_0" /*  */
+
+/* DspBusName List (DspBus Name) */
+#define CRI_MOTOSSOUND_ACF_DSPBUSNAME_MASTEROUT            "MasterOut" /*  */
+
+/* Game Variable (Game Variable Index) */
+#define CRI_MOTOSSOUND_ACF_GAMEVARIABLE_DEFAULT              ( 0) /*  */
+
---
+++ Data/MotosSound/Public/WorkUnit_0/CueSheet_0.h
@@ -0,0 +1,27 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : MotosSound
+ *  Tool Ver.        : Ver.3.40.17
+ *  ACB  Ver.        : Ver.1.31.00
+ *  File Path        : C:/b7n/学校/専門学校2年/workspace/Visual Studio 2015/Projects/3Dプログラム/Motos/Data/MotosSound/Public/WorkUnit_0
+ *  File Name        : CueSheet_0.h
+ *  File Size        : 23200 bytes
+ *  Date Time        : 2020/01/23 11:33:21
+ *  Target           : Public
+ *  Cues             : 2
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : C:/b7n/学校/専門学校2年/workspace/Visual Studio 2015/Projects/3Dプログラム/Motos/Data/MotosSound/Public/WorkUnit_0/CueSheet_0.awb
+ *===========================================================================*/
+
+#define CRI_CUESHEET_0_CUENUM (2)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_CUESHEET_0_CUE_0                ( 0) /*  */
+#define CRI_CUESHEET_0_CUE_1                ( 1) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
